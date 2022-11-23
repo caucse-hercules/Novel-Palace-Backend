000fdf9b1a7422459cb8f8a0589bc4b969d6ebd3
tjwjdgus12 <01048733438a@gmail.com>
2022-11-23 14:23:28 +0900

    init: create spring project

---
+++ src/main/java/com/hercules/novelpalace/NovelPalaceApplication.java
@@ -0,0 +1,13 @@
+package com.hercules.novelpalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelPalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelPalaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hercules/novelpalace/NovelPalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hercules.novelpalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelPalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
