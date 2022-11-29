d5e39ac4c99ac623565bfccb21ece2b6110f8ca3
tjwjdgus12 <01048733438a@gmail.com>
2022-11-29 16:10:26 +0900

    fix: app_name in deploy.sh fixed

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/opt/novel-palace
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=novel-palace
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
