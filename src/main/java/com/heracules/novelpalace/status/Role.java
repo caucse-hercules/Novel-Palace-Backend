bca04cf3b70a1321c2c0bf778edfaecaf01bbe4c
Beopsik <mbsik6082@naver.com>
2022-12-01 15:39:30 +0900

    refactor: modify Role for Spring Security

--- src/main/java/com/heracules/novelpalace/status/Role.java
+++ src/main/java/com/heracules/novelpalace/status/Role.java
@@ -1,6 +1,14 @@
 package com.heracules.novelpalace.status;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    USER,
-    ADMIN
+    USER("ROLE_USER", "회원"),
+    ADMIN("ROLE_ADMIN", "관리자");
+
+    private final String key;
+    private final String title;
 }
