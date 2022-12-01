9fd12e3fd2cd903f32fb7b07730336538ca15921
Beopsik <mbsik6082@naver.com>
2022-12-01 15:43:46 +0900

    feat: add findByEmail method at MemberRepository

--- src/main/java/com/heracules/novelpalace/member/MemberRepository.java
+++ src/main/java/com/heracules/novelpalace/member/MemberRepository.java
@@ -2,5 +2,8 @@ package com.heracules.novelpalace.member;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends MongoRepository<Member, String> {
+    Optional<Member> findByEmail(String email);
 }
