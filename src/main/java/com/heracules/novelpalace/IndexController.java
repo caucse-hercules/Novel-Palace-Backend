d488cd3f22814b6a025095d88712bd5f9baf04bc
Beopsik <mbsik6082@naver.com>
2022-12-02 15:37:11 +0900

    test: add test code about google login

---
+++ src/main/java/com/heracules/novelpalace/IndexController.java
@@ -0,0 +1,24 @@
+package com.heracules.novelpalace;
+
+import com.heracules.novelpalace.config.auth.LoginUser;
+import com.heracules.novelpalace.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser member) {
+        if (member != null) {
+            model.addAttribute("userName", member.getName());
+        }
+        return "index";
+    }
+}
